ac9d85c27e38a021ca1e900e3993f833be6ed149
Semyon Uchvatov <s-uchvatov@yandex.ru>
2020-10-26 17:17:35 +0300

    [Lab 3] parse html table for one file

--- names.py
+++ names.py
@@ -2,12 +2,37 @@ import sys
 from lxml import etree
 import re
 
+allNames = []
+
 
 def extr_name(filename):
     """
     Вход: nameYYYY.html, Выход: список начинается с года, продолжается имя-ранг в алфавитном порядке.
     '2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' и т.д.
     """
+    names = []
+    top = []
+    file = open(filename)
+    year = filename[4:-5]
+    html = file.read()
+    html_table = re.findall(r'<table width="48%".*?</table>', html, flags=re.DOTALL)
+    html_table = re.sub(r'<caption>.*?</caption>', '', html_table[0], flags=re.DOTALL)
+    html_table = re.sub(r'<tr>.*?</table>', '</table>', html_table, flags=re.DOTALL)
+    table = etree.HTML(html_table).find("body/table")
+    rows = iter(table)
+    next(rows)
+    for row in rows:
+        values = [col.text for col in row]
+        top.append(values)
+        names.append((values[0], values[1]))
+        names.append((values[0], values[2]))
+
+    allNames.append((year, top[:10]))
+    names.sort(key=lambda tup: tup[1])
+    print(f"'{year}'", end='')
+    for name in names:
+        print(f", '{name[1]} {name[0]}'", end='')
+    print()
     return
 
 
@@ -18,31 +43,8 @@ def main():
         print('use: [--file] file [file ...]')
         sys.exit(1)
 
-    allNames = []
-
     for arg in args:
-        names = []
-        top = []
-        file = open(arg)
-        year = arg[4:-5]
-        html = file.read()
-        html_table = re.findall(r'<table width="48%".*?</table>', html, flags=re.DOTALL)
-        html_table = re.sub(r'<caption>.*?</caption>', '', html_table[0], flags=re.DOTALL)
-        html_table = re.sub(r'<tr>.*?</table>', '</table>', html_table, flags=re.DOTALL)
-        table = etree.HTML(html_table).find("body/table")
-        rows = iter(table)
-        next(rows)
-        for row in rows:
-            values = [col.text for col in row]
-            top.append(values)
-            names.append((values[0], values[1]))
-            names.append((values[0], values[2]))
-        allNames.append((year, top[:10]))
-        names.sort(key=lambda tup: tup[1])
-        print(f"'{year}'", end='')
-        for name in names:
-            print(f", '{name[1]} {name[0]}'", end='')
-        print()
+        extr_name(arg)
 
     for record in allNames:
         print(record[0] + " top 10:")
