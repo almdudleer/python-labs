487531173385ddc954d02365098b481632e542b4
Alexey Serdyukov <Alexey.Serdyukov@jetbrains.com>
2020-11-09 02:38:40 +0300

    Implement lab4

--- file.py
+++ file.py
@@ -28,11 +28,47 @@ But at least he is not what he used to be
 """
 
 import random
-import sys
+import argparse
+from typing import List, Dict
+
+BOS_WORD = ""
+EOS_WORD = "<EOS>"
 
 
-def mem_dict(filename):
-    return
+def get_similarity_dict(words: List[str]) -> Dict[str, List[str]]:
+    similarity_dict = {}
+    for i, word in enumerate(words):
+        similarity_dict[word] = words[i+1:]
+    return similarity_dict
+
+
+def generate_text(similarity_dict: Dict[str, List[str]], required_len) -> str:
+    generated = []
+    next_word = BOS_WORD
+    i = 0
+    while i < required_len:
+        next_word = random.choice(similarity_dict[next_word])
+        if next_word == EOS_WORD:
+            next_word = BOS_WORD
+            continue
+        i += 1
+        generated.append(next_word)
+    return " ".join(generated)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_file", help="Input file")
+    args = parser.parse_args()
+    with open(args.input_file) as f:
+        words = []
+        for line in f:
+            words += line.strip().split(" ")
+
+    words = [BOS_WORD] + [w for w in words if w] + [EOS_WORD]
+    similarity_dict = get_similarity_dict(words)
+    generated = generate_text(similarity_dict, 40)
+    print(generated)
 
 
 if __name__ == "__main__":
